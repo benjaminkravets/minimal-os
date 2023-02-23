43867370f8fa73301c6f9cb8c9edcb81b068758e
bakravets@gmail.com <bakravets@gmail.com>
2023-02-22 19:08:33 -0500

    add monitor write

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OS
-Workspace for my operating systems project comprised of a shared kernel that boots from GRUB, UEFI, or its own bootloader.
+Workspace for my operating systems project that ties together code from various tutorials and my own kernel code.
+
+Some code used from:
+
+http://www.jamesmolloy.co.uk/tutorial_html/
+
+https://github.com/tuhdo/sample-os
--- custom 32 (idt attempt)/kernel.c
+++ custom 32 (idt attempt)/kernel.c
@@ -1,7 +1,10 @@
-//extern void init_idt();
+extern void init_idt();
+#include "monitor.h"
+#include "common.h"
+
 
 void main () {
-
+init_idt();
 //init_idt () ;
 // Create a pointer to a char , and point it to the first text cell of
 // video memory (i.e. the top - left of the screen )
@@ -14,6 +17,17 @@ char * video_memory2 = ( char*) 0xb8144 ;
 // At the address pointed to by video_memory , store the character ’X’
 // (i.e. display ’X’ in the top - left of the screen ).
   *video_memory2 = 'Y';
+<<<<<<< HEAD
+  
+  monitor_put('n');
+=======
 
-
+idt_flush();
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
+=======
+>>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
+=======
+>>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
 }
