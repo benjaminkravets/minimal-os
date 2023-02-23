146bd30d6ac847334f647364d6c4f241d567ca60
bakravets@gmail.com <bakravets@gmail.com>
2023-02-23 11:47:56 -0500

    organize

--- custom 32 (idt attempt)/kernel.c
+++ custom 32 (idt attempt)/kernel.c
@@ -1,11 +1,8 @@
-extern void init_idt();
-#include "monitor.h"
-#include "common.h"
+
 
 
 void main () {
-init_idt();
-//init_idt () ;
+
 // Create a pointer to a char , and point it to the first text cell of
 // video memory (i.e. the top - left of the screen )
 char * video_memory = ( char*) 0xb8000 ;
@@ -17,17 +14,15 @@ char * video_memory2 = ( char*) 0xb8144 ;
 // At the address pointed to by video_memory , store the character ’X’
 // (i.e. display ’X’ in the top - left of the screen ).
   *video_memory2 = 'Y';
-<<<<<<< HEAD
-  
-  monitor_put('n');
-=======
 
-idt_flush();
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
-=======
->>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
-=======
->>>>>>> 68bd629887ab545c4f3693c5fda009e35bc0a8ac
+  char * video_memory_start = ( char*) 0xba000;
+  *video_memory_start = 'z';
+  for(int i = 0; i < 80*25; i++){
+  
+  }
+  
+
 }
+
+
+
