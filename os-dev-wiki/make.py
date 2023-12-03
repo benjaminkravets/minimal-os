73f926053d7f565e788d37a35d364d9492cd36f1
benjaminkravets <bakravets@gmail.com>
2023-12-02 21:04:00 -0500

    add iso creation routine

--- os-dev-wiki/make.py
+++ os-dev-wiki/make.py
@@ -3,3 +3,7 @@ import os
 os.system("../../bin/i686-elf-as boot.s -o boot.o")
 os.system("../../bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra")
 os.system("../../bin/i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc")
+
+os.system("cp myos.bin isodir/boot/myos.bin")
+os.system("cp grub.cfg isodir/boot/grub/grub.cfg")
+os.system("grub-mkrescue -o myos.iso isodir")
