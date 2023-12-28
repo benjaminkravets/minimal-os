d069d8a005043899ce7490c559e06f92008512be
Benjamin Kravets <bakravets@gmail.com>
2023-12-27 19:49:59 -0500

    test push

--- os-dev-wiki-uefi/make.py
+++ os-dev-wiki-uefi/make.py
@@ -6,5 +6,4 @@ os.system("x86_64-w64-mingw32-gcc -ffreestanding -I../../gnu-efi/inc/ -I../../gn
 #Link resulting object files into EFI file
 os.system("x86_64-w64-mingw32-gcc -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o BOOTX64.EFI hello.o data.o")
 
-#os.system("clang -target x86_64-unknown-windows -ffreestanding -fshort-wchar -mno-red-zone -I../../gnu-efi/inc/ -I../../gnu-efi/inc/x86_64/ -I../../gnu-efi/inc/protocol/ -c -o hello.o hello.c")
 
