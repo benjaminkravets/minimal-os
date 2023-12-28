31eb4586da0380af3294ad97d2ad79838e898fbb
Benjamin Kravets <bakravets@gmail.com>
2023-12-27 19:50:47 -0500

    finish uefi build

--- os-dev-wiki-uefi/make.py
+++ os-dev-wiki-uefi/make.py
@@ -6,4 +6,13 @@ os.system("x86_64-w64-mingw32-gcc -ffreestanding -I../../gnu-efi/inc/ -I../../gn
 #Link resulting object files into EFI file
 os.system("x86_64-w64-mingw32-gcc -nostdlib -Wl,-dll -shared -Wl,--subsystem,10 -e efi_main -o BOOTX64.EFI hello.o data.o")
 
+#create fat filesystem
+os.system("dd if=/dev/zero of=fat.img bs=1k count=1440")
+os.system("mformat -i fat.img -f 1440 ::")
+os.system("mmd -i fat.img ::/EFI")
+os.system("mmd -i fat.img ::/EFI/BOOT")
+os.system("mcopy -i fat.img BOOTX64.EFI ::/EFI/BOOT")
 
+os.system("cp fat.img iso")
+os.system("xorriso -as mkisofs -R -f -e fat.img -no-emul-boot -o cdimage.iso iso")
+os.system("sudo qemu-system-x86_64 -L /usr/share/qemu -pflash /usr/share/qemu/OVMF.fd -cdrom cdimage.iso")
